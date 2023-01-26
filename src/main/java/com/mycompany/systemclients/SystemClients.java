51a1596b73258aff90c28a64b2e0d1a21188f1de
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-01-26 11:05:53 -0300

    first commit

---
+++ src/main/java/com/mycompany/systemclients/SystemClients.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.systemclients;
+
+/**
+ *
+ * @author pachi
+ */
+public class SystemClients {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.show();
+    }
+}
